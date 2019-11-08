39b719010e5f183d9b04e128eb6420131d9241e7
Ezequiel <ezequiel.domenech@gmail.com>
2019-11-08 13:59:14 -0300

    agregada la creacion de la tabla default_files

--- customupload.php
+++ customupload.php
@@ -101,6 +101,7 @@ function cu_create_table(){
         dbDelta( $sql );
 
     }
+
     $table_name = $cudb->prefix.'cu_clientes';
 
     if( $cudb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name ) {
@@ -118,6 +119,23 @@ function cu_create_table(){
 
     }
 
+    $table_name = $cudb->prefix.'cu_default_files';
+
+    if( $cudb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name ) {
+
+        $charset_collate = $cudb->get_charset_collate();
+
+        $sql = "CREATE TABLE $table_name (
+            file_id bigint(20) NOT NULL AUTO_INCREMENT,
+            file_dir varchar(120) NOT NULL,
+            file_type varchar(120) NOT NULL,
+            PRIMARY KEY  (file_id)
+        ) $charset_collate;";
+
+        require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+        dbDelta( $sql );
+
+    }
 }
 
 function cu_install(){
