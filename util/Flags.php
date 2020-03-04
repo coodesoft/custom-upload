cd90297a2a3430516d2564621f611c4a2835023e
Laion <maikndawer@gmail.com>
2020-03-04 02:21:53 -0300

    se agregó soporte para flags de estado

---
+++ util/Flags.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class Flags{
+
+    const UPLOAD_SUCCESS                = '1000';
+
+    const COPY_FILE_ERROR               = '1001';
+
+    const DB_SAVE_EXCEPTION             = '1002';
+
+    const DB_SAVE_ERROR                 = '1003';
+
+    const CREATE_DIR_ERROR              = '1004';
+
+
+}
