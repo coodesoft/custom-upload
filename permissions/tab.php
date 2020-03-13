63474177f04b37adfdf3b2b7d7098500d4acf2ad
Leonardo <maikndawer@gmail.com>
2020-03-13 18:52:07 -0300

    se actualizó la interfaz para que muestre mensajes mas puntuales sobre los resultados de las operaciones

--- permissions/tab.php
+++ permissions/tab.php
@@ -11,9 +11,30 @@ function assignCapabilities(){
     <p>Seleccione un cliente y luego haga click en cada checkbox para indicar que se otorga el permiso de descarga. </p>
   </div>
 
-  <?php if (isset($_GET['assign_status'])){ ?>
+  <?php if (isset($_GET['assign_status'])){
+    $assign_status = $_GET['assign_status'];
+  ?>
   <div id="actionResult">
-    <p><?php echo $result = $_GET['assign_status'] ? 'Se modificaron los permisos de descarga exitosamente':'Se produjo un error inesperado al modificar los permisos de descarga'?></p>
+    <?php if ($assign_status == Flags::ASSIGN_PERMISSON_SUCCESS) {?>
+      <p>Se modificaron los permisos de descarga exitosamente.</p>
+    <?php } ?>
+
+    <?php if ($assign_status == Flags::ASSIGN_DELETE_ERROR) {?>
+      <p>Se produjo un error al eliminar permisos. La operación no se pudo completar.</p>
+    <?php } ?>
+
+    <?php if ($assign_status == Flags::ASSIGN_DEFUALT_ERROR) {?>
+      <p>Se modificaron los permisos exitosamente, pero se produjo un erro al actualizar el estado de default de los archivos asociados. La operación no se pudo completar.</p>
+    <?php } ?>
+
+    <?php if ($assign_status == Flags::ASSIGN_ADD_ERROR) {?>
+      <p>Se produjo un error al agregar permisos. La operación no se pudo completar.</p>
+    <?php } ?>
+
+    <?php if ($assign_status == Flags::DB_PARAM_ERROR) {?>
+      <p>Se encontró un error en el envío de parámetros al servicio encargado de actualizar los permisos. La operación no se pudo completar.</p>
+    <?php } ?>
+
   </div>
   <?php } ?>
 
